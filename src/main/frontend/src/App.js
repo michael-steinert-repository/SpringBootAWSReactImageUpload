50bb8d7facb67c2ee3b3d00538961e8200054538
Michael Steinert <steinert-michael@gmx.de>
2020-04-01 22:11:28 +0200

    added dropzone to frontend

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,19 +1,70 @@
-import React, {useState, useEfect} from 'react';
+import React, {useState, useEfect, useCallback} from 'react';
 import './App.css';
 import axios from "axios";
+import {useDropzone} from 'react-dropzone'
 
 const userProfiles = () => {
+
+  const [userProfiles, setUserProfiles] = useState([]);
+
   const fetchUserProfiles = () => {
     axios.get("http://localhost:8080/api/v1/user-profile").then(res => {
           console.log(res);
+          setUserProfiles(res.data)
     });
-  }
+  };
   useEfect(() => {
     fetchUserProfiles();
   }, []);
 
-  return
+  return userProfiles.map((userProfile, index) => {
+    return (
+        <div key={index}>
+            <br/>
+            <h1>{userProfile.username}</h1>
+            <p>{userProfile.userProfileId}</p>
+            <Dropzone {...userProfile} />
+            /*alternativ: <Dropzone userProfileId={userProfile.userProfileId} /> */
+            <br/>
+        </div>
+    );
+  });
 };
+
+function Dropzone({userProfileId}) {
+    const onDrop = useCallback(acceptedFiles => {
+        const file = acceptedFiles[0];
+
+        console.log(file);
+
+        const formData = new FormData();
+        formData.append("file", file);
+
+        axios.post(`http://localhost:8080/api/v1/user-profile/${userProfileId}/image/upload`,
+            formData,
+            {
+                headers: {
+                    "Content-Type" : "multipart/form-data"
+                }
+            }).then(() => {
+                console.log("file uploaded successfully");
+        }).catch(err => {
+            console.log(err);
+        });
+    }, []);
+    const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop});
+
+    return (
+        <div {...getRootProps()}>
+            <input {...getInputProps()} />
+            {
+                isDragActive ?
+                    <p>Drop the profile image here ...</p> :
+                    <p>Drag 'n' drop profile image here, or click to select profile image</p>
+            }
+        </div>
+    );
+}
 
 function App() {
   return (
--- src/main/java/com/example/SpringBootAWSReactImageUpload/profile/UserProfileService.java
+++ src/main/java/com/example/SpringBootAWSReactImageUpload/profile/UserProfileService.java
@@ -1,20 +1,25 @@
 package com.example.SpringBootAWSReactImageUpload.profile;
 
+import com.example.SpringBootAWSReactImageUpload.bucket.BucketName;
+import com.example.SpringBootAWSReactImageUpload.filestore.FileStore;
+import org.apache.http.entity.ContentType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.util.List;
-import java.util.UUID;
+import java.io.IOException;
+import java.util.*;
 
 @Service
 public class UserProfileService {
 
     private final UserProfileDataAccessService userProfileDataAccessService;
+    private final FileStore fileStore;
 
     @Autowired
-    public UserProfileService(UserProfileDataAccessService userProfileDataAccessService) {
+    public UserProfileService(UserProfileDataAccessService userProfileDataAccessService, FileStore fileStore) {
         this.userProfileDataAccessService = userProfileDataAccessService;
+        this.fileStore = fileStore;
     }
 
     public List<UserProfile> getUserProfiles() {
@@ -22,10 +27,31 @@ public class UserProfileService {
     }
 
     public void uploadUserProfileImage(UUID userProfileId, MultipartFile file) {
-        //1. Check if image is not empty
-        //2. If file is an image
-        //3. The user exists in our database
-        //4. Grab some metadata from file if any
-        //5. Store the image in s3 and update database with s3 image link
+        if(file.isEmpty()){
+            throw new IllegalStateException("Cannot upload empty file [" + file.getSize() + "]");
+        }
+
+        if(!Arrays.asList(ContentType.IMAGE_JPEG, ContentType.IMAGE_PNG, ContentType.IMAGE_GIF).contains(file.getContentType())) {
+            throw new IllegalStateException("File must be an image");
+        }
+
+        UserProfile user = userProfileDataAccessService
+                .getUserProfiles()
+                .stream()
+                .filter(userProfile -> userProfile.getUserProfileId().equals(userProfileId))
+                .findFirst()
+                .orElseThrow(() -> new IllegalStateException((String.format(("User profile %s not found"), userProfileId))));
+
+        Map<String, String> metadata = new HashMap<>();
+        metadata.put("Content-Type", file.getContentType());
+        metadata.put("Content-Length", String.valueOf(file.getSize()));
+
+        String path = String.format("%s/%s", BucketName.PROFILE_IMAGE.getBucketName(), user.getUserProfileId());
+        String filename = String.format("%s-%s", file.getName(), UUID.randomUUID());
+        try {
+            fileStore.save(path, filename, Optional.of(metadata), file.getInputStream());
+        } catch (IOException e) {
+            throw new IllegalStateException(e);
+        }
     }
 }
